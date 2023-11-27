c9cb6125e1f5133b731a98142bc542c23981d05c
Gussak <Gussak@users.noreply.github.com>
2023-11-27 20:03:58 -0300

    Update autoSIGSTOPonLostFocus.sh

--- bin/autoSIGSTOPonLostFocus.sh
+++ bin/autoSIGSTOPonLostFocus.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 #helps cool down cpu on slow PCs
+: ${nMinCpuTime:=3} #help
 while true;do 
 	nWPid="$(xdotool getwindowpid $(xdotool getactivewindow))";
 	nPid="$(pgrep -f "^[.]/arx ")";
-	ps -p $nWPid $nPid&&:;
+	declare -p nWPid nPid&&:
+	ps -o pid,cputimes,stat,cmd -p $nWPid $nPid&&:
 	
 	if [[ -n "$nWPid" ]] && [[ -n "$nPid" ]];then
-		if((nPid==nWPid));then 
+		nCpuTime=$(ps --no-headers -o cputimes -p $nPid |tr -d ' ') 
+		if((nPid==nWPid || nCpuTime<nMinCpuTime));then #nCpuTime is to let the window open
 			kill -SIGCONT $nPid;
-		else 
-			kill -SIGSTOP $nPid;
+		else
+			if((nCpuTime >= nMinCpuTime));then
+				kill -SIGSTOP $nPid;
+			fi
 		fi;
 	fi
 	sleep 2;
