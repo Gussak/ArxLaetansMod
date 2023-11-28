ce9ed8b5fec872dca8b026e01754845ff720410f
Gussak <Gussak@users.noreply.github.com>
2023-11-27 21:52:25 -0300

    Update gitgui.sh

--- bin/gitgui.sh
+++ bin/gitgui.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 cd "`dirname "$0"`"
-(xterm -e git gui & disown)
+#(xterm -e git gui & disown)
+#(git gui & disown)
+(xterm -e bash -c "sleep 1;(nohup git gui & disown);sleep 1"&disown)
 read -t 60 -n 1 -p PressAKeyToExit
