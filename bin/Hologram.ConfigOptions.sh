e8404357171864cddd15796ecc59a83623a1e5b9
Gussak <Gussak@users.noreply.github.com>
2023-12-15 06:43:36 -0300

    fixed hologram cfg

--- bin/Hologram.ConfigOptions.sh
+++ bin/Hologram.ConfigOptions.sh
@@ -19,8 +19,8 @@ function FUNCaddOpt() { # index cfgVarID comment
 }
 #obs.: options doesnt need to have the same name size it is cohincidence for now...
 FUNCaddOpt 17 ClassFocus "(TODO/WIP)"
-FUNCaddOpt 58 ShowLocals "(DEBUG)"
-FUNCaddOpt 59 DebugTests "(DEBUG)"
+FUNCaddOpt 21 ShowLocals "(DEBUG)"
+FUNCaddOpt 22 DebugTests "(DEBUG)"
 SECFUNCarrayShow -v astrOptions
 SECFUNCarrayShow -v astrCfgVar
 
@@ -34,14 +34,35 @@ nRmSpaces=1 #before the %
 ((nRmSpaces+=2))&&: #the ') '
 ((nRmSpaces+=1))&&: #was needed...
 
-iColumn1=$(((nItemsPerFace*1)+1))
-iColumn2=$(((nItemsPerFace*2)+1))
-iColumn3=$(((nItemsPerFace*3)+1))
-iColumn4=$(((nItemsPerFace*4)+1))
+: ${bSwapColumnRightWithBottom:=true} #help
 
-: ${iStartIndex:=1} #help can add pages later TODO
-for((i=iStartIndex;i<=nTot;i++));do
-	#strOpt="${astrOptions[$i]-}"
+iFront=$(((nItemsPerFace*0)+1))
+iBottomDefault=$(((nItemsPerFace*5)+1))
+iBottom=$iBottomDefault
+iColumn1=$(((nItemsPerFace*1)+1)) #front face
+iColumn2Default=$(((nItemsPerFace*2)+1)) #right face
+iColumn2=$iColumn2Default
+if $bSwapColumnRightWithBottom;then 
+	iColumn2=$iBottomDefault
+	iBottom=$iColumn2Default
+fi
+iColumn3=$(((nItemsPerFace*3)+1)) #back face
+iColumn4=$(((nItemsPerFace*4)+1)) #left face
+
+: ${iStartIndex:=1} #help can add pages later TODO not working yet
+: ${bReverseOrder:=false} #help TODO if true is not working yet
+if $bReverseOrder;then iControl=$nTot;else iControl=$iStartIndex;fi
+#for((iControl=iStartIndex;iControl<=nTot;iControl++));do
+#for((iControl=nTot;iControl>=iStartIndex;iControl--));do
+while true;do
+	if $bReverseOrder;then  #loop control
+		if ! ((iControl>=iStartIndex));then break;fi
+	else
+		if ! ((iControl<=nTot));then break;fi
+	fi
+	# FIRST THING ABOVE: LOOP CONTROL
+	
+	#strOpt="${astrOptions[$iControl]-}"
 	#if(( (${#strOpt}+nRmSpaces) > ${#strSpacesFaceFullW} ));then 
 		#declare -p nRmSpaces strOpt strSpacesFaceFullW |sed -r 's@="@\n"@' >&2
 		#echo "$(printf "\"%$((nRmSpaces+${#strOpt}))s\"" "${strOpt}")" >&2
@@ -49,36 +70,38 @@ for((i=iStartIndex;i<=nTot;i++));do
 		#exit 1;
 	#fi
 	
-	bTop=false;if((i<=nItemsPerFace));then bTop=true;fi
-	bBottom=false;if((i>(nTot-nItemsPerFace)));then bBottom=true;fi
+	bTop=false;if((iControl<=nItemsPerFace));then bTop=true;fi
+	bBottom=false;if((iControl>(nTot-nItemsPerFace)));then bBottom=true;fi
 	
 	nAdjustRmSp=0;if ! $bTop && ! $bBottom;then nAdjustRmSp=1;fi
 	
 	#strOptOk="${strOpt}${strSpacesFaceFullW:$((${#strOpt}+nRmSpaces+nAdjustRmSp))}"
 	
 	if $bTop || $bBottom;then
-		iIndexForColumn=$i
+		iWork=$iControl
+		if((iWork>=iBottomDefault));then iWork=$((iWork-iBottomDefault+iBottom));fi
+		iIndexForColumn=$iWork
 		strSpacesBefore="${strSpacesFaceFullW}"
 		strNL=""
-		#if((i==(nTot-nItemsPerFace+1)));then strNL+="${strEmptyLineWithNL}";fi #fist bottom line
+		#if((iWork==(nTot-nItemsPerFace+1)));then strNL+="${strEmptyLineWithNL}";fi #fist bottom line
 		if $bTop;then strNL+="\n${strEmptyLineWithNL}";fi
 		if $bBottom;then strNL+="\n${strEmptyLineWithNL}";fi
-		#if((i==nItemsPerFace));then strNL+="${strEmptyLineWithNL}";fi #last top line
+		#if((iWork==nItemsPerFace));then strNL+="${strEmptyLineWithNL}";fi #last top line
 		#printf "${strSpacesBefore}%02d) ${strOptOk}${strNL}" $iIndexForColumn;
 		#echo " " #an empty line with a single space to separate them clearly
 	else # left front right back
 		strSpacesBefore=" "
-		#if(((i+nItemsPerFace-2)%4 == 2));then echo -n " ";fi; #before column1 in the line
-		if(((i+nItemsPerFace-2)%4 == 2));then strSpacesBefore+=" ";fi; #before column1 in the line
+		#if(((iWork+nItemsPerFace-2)%4 == 2));then echo -n " ";fi; #before column1 in the line
+		if(((iControl+nItemsPerFace-2)%4 == 2));then strSpacesBefore+=" ";fi; #before column1 in the line
 		
-		if(((i+nItemsPerFace-2)%4 == 2));then iIndexForColumn=$((iColumn1++));fi #column 1
-		if(((i+nItemsPerFace-2)%4 == 3));then iIndexForColumn=$((iColumn2++));fi #column 2
-		if(((i+nItemsPerFace-2)%4 == 0));then iIndexForColumn=$((iColumn3++));fi #column 3
-		if(((i+nItemsPerFace-2)%4 == 1));then iIndexForColumn=$((iColumn4++));fi #column 4
+		if(((iControl+nItemsPerFace-2)%4 == 2));then iIndexForColumn=$((iColumn1++));fi #column 1
+		if(((iControl+nItemsPerFace-2)%4 == 3));then iIndexForColumn=$((iColumn2++));fi #column 2
+		if(((iControl+nItemsPerFace-2)%4 == 0));then iIndexForColumn=$((iColumn3++));fi #column 3
+		if(((iControl+nItemsPerFace-2)%4 == 1));then iIndexForColumn=$((iColumn4++));fi #column 4
 		
-		#if((i>nItemsPerFace));then printf " %02d) ${strOptOk}" $((iIndexForColumn));fi;
+		#if((iWork>nItemsPerFace));then printf " %02d) ${strOptOk}" $((iIndexForColumn));fi;
 		strNL=""
-		if(( (i+nItemsPerFace-2)%4 == 0 ));then 
+		if(( (iControl+nItemsPerFace-2)%4 == 0 ));then 
 			#new line at end of the line
 			strNL="\n${strEmptyLineWithNL}"
 		fi; 
@@ -94,42 +117,40 @@ for((i=iStartIndex;i<=nTot;i++));do
 	strOptOk="${strOpt}${strSpacesFaceFullW:$((${#strOpt}+nRmSpaces+nAdjustRmSp))}"
 	printf "${strSpacesBefore}%02d) ${strOptOk}${strNL}" $iIndexForColumn;
 	
-	if((i>1));then
+	if((iWork>1));then
 		strPrefixPng="./Hologram.ConfigOptions.index"
 		if [[ -f "${strPrefixPng}00001.Clear.png" ]];then
-			ln -vsf "${strPrefixPng}00001.Clear.png"   "${strPrefixPng}`printf %05d $i`.Clear.png"     >&2
+			ln -vsf "${strPrefixPng}00001.Clear.png"   "${strPrefixPng}`printf %05d $iWork`.Clear.png"     >&2
 		fi
 		if [[ -f "${strPrefixPng}00001.Enabled.png" ]];then
-			ln -vsf "${strPrefixPng}00001.Enabled.png"   "${strPrefixPng}`printf %05d $i`.Enabled.png"   >&2
+			ln -vsf "${strPrefixPng}00001.Enabled.png"   "${strPrefixPng}`printf %05d $iWork`.Enabled.png"   >&2
 		fi
 		if [[ -f "${strPrefixPng}00001.Highlight.png" ]];then
-			ln -vsf "${strPrefixPng}00001.Highlight.png" "${strPrefixPng}`printf %05d $i`.Highlight.png" >&2
+			ln -vsf "${strPrefixPng}00001.Highlight.png" "${strPrefixPng}`printf %05d $iWork`.Highlight.png" >&2
 		fi
 	fi
 	
 	# validate the script
-	if [[ -n "${astrCfgVar[$i]-}" ]];then 
-		strCodeGenUpdateCfgOpt+="		Set @CFUNCconfigOptionUpdate_check ${astrCfgVar[$i]} GoSub CFUNCconfigOptionUpdate\n"
-		strCodeGenDefaultsCfgOpt+="	if(${astrCfgVar[$i]} == 0) Set ${astrCfgVar[$i]} ${i}.0\n"
+	if [[ -n "${astrCfgVar[$iControl]-}" ]];then 
+		strCodeGenUpdateCfgOpt+="		Set @CFUNCconfigOptionUpdate_check ${astrCfgVar[$iControl]} GoSub CFUNCconfigOptionUpdate\n"
+		strCodeGenDefaultsCfgOpt+="	if(${astrCfgVar[$iControl]} == 0) Set ${astrCfgVar[$iControl]} ${iControl}.0\n"
 		
-		strRegexVar="`echo "${astrCfgVar[$i]}" |sed 's@.@[&]@g'`" 
-		echo "VALIDATING[$i]: ${astrCfgVar[$i]} '$strRegexVar'" >&2;
+		strRegexVar="`echo "${astrCfgVar[$iControl]}" |sed 's@.@[&]@g'`" 
+		echo "VALIDATING[$iControl]: ${astrCfgVar[$iControl]} '$strRegexVar'" >&2;
 		egrep "${strRegexVar}" "${strFlHolog}" -ai >&2
 		strRegexChk="${strRegexVar}[ =!><]*"
-		if egrep "${strRegexChk}.*[0-9]" "${strFlHolog}" -ai |egrep -v "${strRegexChk}(0|${i})" -wai >&2;then
-			echoc -p "Invalid use at .asl script (${astrCfgVar[$i]}, $i)" >&2
+		echo ">>--> chk >>-->" >&2
+		if egrep "${strRegexChk}.*[0-9]" "${strFlHolog}" -ai |egrep -v "${strRegexChk}(0|${iControl})" -wai >&2;then
+			echoc -p "A: Invalid use at .asl script (${astrCfgVar[$iControl]}, $iControl)" >&2
 		fi
-		#if ! egrep "[\t ]*if\(${strRegexVar} == 0\) Set ${strRegexVar} ${i}[.]0" "${strFlHolog}" -a >&2;then
-			#echoc -p "The above line is missing at .asl script (${astrCfgVar[$i]}, $i)" >&2
-		#fi
-		#if egrep "${strRegexVar} *[=!><]* *" "${strFlHolog}" -ain |egrep -vaw "^[0-9]*:.*${strRegexVar} [=!><] (0|${i})" >&2;then
-			#echoc -p "invalid comparison at .asl script (${astrCfgVar[$i]}, $i)"
-		#fi
-		#if egrep "Set ${strRegexVar} " "${strFlHolog}" -ain |egrep -vaw "^[0-9]*:.*Set ${strRegexVar} (0|${i})" >&2;then # usually a comparison like ... == nn or an assignment like set ... nn
-			#echoc -p "invalid assignment at .asl script (${astrCfgVar[$i]}, $i)"
-		#fi
+		if egrep "${strRegexChk}.*[0-9]" "${strFlHolog}" -ai |egrep -v "${strRegexChk}${iControl}" -wai >&2;then
+			echoc -p "B: Invalid use at .asl script (${astrCfgVar[$iControl]}, $iControl)" >&2
+		fi
 		echo >&2
 	fi
+	
+	#LAST THING: loop control
+	if $bReverseOrder;then ((iControl--))&&:;else ((iControl++))&&:;fi #loop control
 done >"$0.txt"
 
 echo "TOKEN_AUTOPATCH_strCodeGenUpdateCfgOpt_BEGIN"
