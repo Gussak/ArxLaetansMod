941ccd092c41961f998df199e562021a8d5985e5
gussak <ghussack@gmail.com>
2023-12-24 02:13:54 -0300

    updating scripts

--- bin/createArxLaetans.SnapShot.sh
+++ bin/createArxLaetans.SnapShot.sh
@@ -2,16 +2,8 @@
 
 source <(secinit)
 
-strBPath="ArxLaetansMod.github" createArxLibertatis.ForArxLaetans.SnapShot.sh
+echoc -p "not working yet"
+echoc -w
+exit 1
 
-#strFlBN="ArxLaetansMod.SnapShot.`SECFUNCdtFmt --filename`"
-
-#tar \
-#	--exclude="ArxLaetansMod.github/build" \
-#	--exclude="ArxLaetansMod.github/.git" \
-#	-vcf "${strFlBN}.tar" \
-#	ArxLaetansMod.github/*
-
-#7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -mmt16 "${strFlBN}.tar.7z" "${strFlBN}.tar"
-
-#trash "${strFlBN}.tar"
+strBPath="ArxLaetansMod.github" ./createArxLibertatis.ForArxLaetans.SnapShot.sh
