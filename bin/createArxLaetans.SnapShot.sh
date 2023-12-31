9f3676787fcf7760c7c3ba04823f1f9189e96720
gussak <ghussack@gmail.com>
2023-12-31 02:50:27 -0300

    updated scripts

--- bin/createArxLaetans.SnapShot.sh
+++ bin/createArxLaetans.SnapShot.sh
@@ -1,9 +1,44 @@
 #!/bin/bash
 
+egrep "[#]help" "$0"
+
 source <(secinit)
 
-echoc -p "not working yet"
-echoc -w
-exit 1
+: ${bCompiledMode:=false} #help
 
-strBPath="ArxLaetansMod.github" ./createArxLibertatis.ForArxLaetans.SnapShot.sh
+: ${strBPath:="ArxLaetansMod.github"} #help
+
+#IFS=$'\n' read -d '' -r -a astrFileList < <(cat "${strBPath}/.gitignore" |sed "s@^#[!]@!@" |egrep -v "/$|^$|^#|^[*]$" |sort -u |tr -d '!' |sed 's@.*@--include="&"@')&&:
+#IFS=$'\n' read -d '' -r -a astrFileList < <(cat "${strBPath}/.gitignore" |sed "s@^#[!]@!@" |egrep -v "/$|^$|^#|^[*]$" |sort -u |tr -d '!' |sed "s@.*@${strBPath}/&@")&&:
+IFS=$'\n' read -d '' -r -a astrRegexFileList < <(cat "${strBPath}/.gitignore" |sed "s@^#[!]@!@" |egrep -v "/$|^$|^#|^[*]$" |sort -u |tr -d '!' |sed -r -e 's@[*]@.*@g' -e "s@.*@${strBPath}/&@")&&:
+declare -p astrRegexFileList |tr '[' '\n'
+echoc -w "prepare find"
+
+astrFindRegexFileList=()
+for strRegexFile in "${astrRegexFileList[@]}";do
+	if((`SECFUNCarraySize astrFindRegexFileList` > 0));then
+		astrFindRegexFileList+=(-or)
+	fi
+	astrFindRegexFileList+=(-iregex)
+	astrFindRegexFileList+=("$strRegexFile")
+done
+declare -p astrFindRegexFileList |tr '[' '\n'
+IFS=$'\n' read -d '' -r -a astrFileList < <(find "${strBPath}/" "${astrFindRegexFileList[@]}")&&:
+echoc -w "prepare tar"
+
+strBranch="$(cd "${strBPath}";git branch |egrep "^[*]" |cut -f2 -d' ')"
+
+strWhat="ArxLaetans"
+astrTarParams=(
+	#--exclude="${strBPath}/build/CMakeFiles"
+	#--exclude="${strBPath}/build"
+	#--exclude="${strBPath}/.git"
+)
+
+strFlBN="${strBPath}.ForArxLaetansMod.${strWhat}.Branch_${strBranch}.SnapShot.`SECFUNCdtFmt --filename`"
+
+SECFUNCexecA -ce tar "${astrTarParams[@]}" -vcf "${strFlBN}.tar" "${astrFileList[@]}"
+
+7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -mmt16 "${strFlBN}.tar.7z" "${strFlBN}.tar"
+
+trash "${strFlBN}.tar"
