4cf46401b59d75d4835168d1062b55ae6ac8b9d3
gussak <ghussack@gmail.com>
2024-01-01 20:13:46 -0300

    updated bash script

--- bin/cmp3way.sh
+++ bin/cmp3way.sh
@@ -21,9 +21,9 @@ declare -p strFl3
 echoc --info "Step:C) final PR branch 3rd, to copy changes to. Copy here ONLY diffs from the 1st to the 2nd!!!"
 echo
 
-if [[ ! -f "$strFl1" ]];then echoc -p "invalid strFl1='$strFl1'";exit 1;fi
-if [[ ! -f "$strFl2" ]];then echoc -p "invalid strFl2='$strFl2'";exit 1;fi
-if [[ ! -f "$strFl3" ]];then echoc -p "invalid strFl3='$strFl3'";exit 1;fi
+if [[ ! -f "$strFl1" ]];then echoc -p "invalid strFl1='$strFl1'";echoc -w;exit 1;fi
+if [[ ! -f "$strFl2" ]];then echoc -p "invalid strFl2='$strFl2'";echoc -w;exit 1;fi
+if [[ ! -f "$strFl3" ]];then echoc -p "invalid strFl3='$strFl3'";echoc -w;exit 1;fi
 
 echoc -w "compare 3-way with meld"
 meld "$strFl1" "$strFl2" "$strFl3"
