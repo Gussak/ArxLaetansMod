7a683c292a268d688da61ada1ded38d70dba530a
Gussak <Gussak@users.noreply.github.com>
2024-01-06 14:19:44 -0300

    Update createArxLaetans.SnapShot.sh

--- bin/createArxLaetans.SnapShot.sh
+++ bin/createArxLaetans.SnapShot.sh
@@ -12,7 +12,8 @@ source <(secinit)
 #IFS=$'\n' read -d '' -r -a astrFileList < <(cat "${strBPath}/.gitignore" |sed "s@^#[!]@!@" |egrep -v "/$|^$|^#|^[*]$" |sort -u |tr -d '!' |sed "s@.*@${strBPath}/&@")&&:
 IFS=$'\n' read -d '' -r -a astrRegexFileList < <(cat "${strBPath}/.gitignore" |sed "s@^#[!]@!@" |egrep -v "/$|^$|^#|^[*]$" |sort -u |tr -d '!' |sed -r -e 's@[*]@.*@g' -e "s@.*@${strBPath}/&@")&&:
 declare -p astrRegexFileList |tr '[' '\n'
-echoc -w "prepare find"
+
+echoc --info "preparing find regex from .gitignore"
 
 astrFindRegexFileList=()
 for strRegexFile in "${astrRegexFileList[@]}";do
@@ -24,7 +25,21 @@ for strRegexFile in "${astrRegexFileList[@]}";do
 done
 declare -p astrFindRegexFileList |tr '[' '\n'
 IFS=$'\n' read -d '' -r -a astrFileList < <(find "${strBPath}/" "${astrFindRegexFileList[@]}")&&:
-echoc -w "prepare tar"
+
+: ${strFileFilter:=""} #help to backup only text files, try strFileFilter=".*[.](txt|asl|info|md|sh|py|cfg|patch|d)"
+if [[ -n "${strFileFilter}" ]];then
+	echoc --info "applying filter strFileFilter='$strFileFilter'"
+	astrFileListTmp=("${astrFileList[@]}")
+	astrFileList=()
+	for strFile in "${astrFileListTmp[@]}";do
+		if [[ "$strFile" ~= ${strFileFilter} ]];then
+			astrFileList+="${strFile}"
+		fi
+	done
+fi
+declare -p astrFileList |tr '[' '\n'
+
+echoc --info "creating tar"
 
 strBranch="$(cd "${strBPath}";git branch |egrep "^[*]" |cut -f2 -d' ')"
 
@@ -38,6 +53,8 @@ astrTarParams=(
 strFlBN="${strBPath}.ForArxLaetansMod.${strWhat}.Branch_${strBranch}.SnapShot.`SECFUNCdtFmt --filename`"
 
 SECFUNCexecA -ce tar "${astrTarParams[@]}" -vcf "${strFlBN}.tar" "${astrFileList[@]}"
+
+echoc --info "compressing"
 
 7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -mmt16 "${strFlBN}.tar.7z" "${strFlBN}.tar"
 
