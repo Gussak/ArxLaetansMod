2e8a5f6cbebc239cd19e2c10865b0b85d39492ad
gussak <ghussack@gmail.com>
2024-02-11 20:35:21 -0300

    update scripts

--- bin/buildSimple.sh
+++ bin/buildSimple.sh
@@ -5,3 +5,4 @@ cd build
 cmake -DDEVELOPER=ON ..
 if echoc -q "check style?";then make style;fi
 while ! make -j "`grep "core id" /proc/cpuinfo |wc -l`";do echoc -w retry;done
+if echoc -q "copy executable to deploy folder?";then echoc -x cp -vf arx "../../../ArxLibertatis.layer7057.CoreNewerThanOverhaul-arx-libertatis-1.3-dev-2023-06-24-LinuxBuild/";fi
