f5940010bd41f8ad833d479e0e4dd7897a183a14
gussak <ghussack@gmail.com>
2024-02-12 02:27:25 -0300

    upd scripts

--- bin/buildSimple.sh
+++ bin/buildSimple.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+: ${bFast:=false} #help
+fWaitTime=9999; if $bFast;then fWaitTime=0.01;fi
+
 set -Eeu
+
 mkdir -vp build
 cd build
 cmake -DDEVELOPER=ON ..
-if echoc -q "check style?";then make style;fi
-while ! make -j "`grep "core id" /proc/cpuinfo |wc -l`";do echoc -w retry;done
-if echoc -q "copy executable to deploy folder?";then echoc -x cp -vf arx "../../../ArxLibertatis.layer7057.CoreNewerThanOverhaul-arx-libertatis-1.3-dev-2023-06-24-LinuxBuild/";fi
+
+if echoc -t $fWaitTime -q "check style?";then make style;fi
+
+if ! make -j "`grep "core id" /proc/cpuinfo |wc -l`";then exit 1;fi
+
+if echoc -t $fWaitTime -q "copy executable to deploy folder?@Dy";then echoc -x cp -vf arx "../../../ArxLibertatis.layer7057.CoreNewerThanOverhaul-arx-libertatis-1.3-dev-2023-06-24-LinuxBuild/";fi
