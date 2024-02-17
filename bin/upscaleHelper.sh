5af014048ea7ab53ec520efb60003b66eb8b4efc
gussak <ghussack@gmail.com>
2024-02-17 16:03:05 -0300

    updating scripts and docs

---
+++ bin/upscaleHelper.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+egrep "[#]help" "$0"
+
+set -Eeu
+
+if [[ "$1" == "--dupscale" ]];then #help <w> <h> min "OR" sizes to accept file
+	shift
+	export wChk=$1;shift
+	export hChk=$1;shift
+	
+	FUNCdupscayl() {
+		fl="$1";
+		
+		szCode="$(identify "$fl" |sed -r 's@.* PNG ([0-9]*)x([0-9]*) .*@w=\1;h=\2;@' )";
+		if [[ -z "${szCode-}" ]];then return;fi
+		eval "$szCode";
+		#declare -p fl szCode w h wChk hChk
+		
+		if((w <= wChk || h <= hChk));then
+			echo "mv -f \"${fl}\" ./ # $szCode" |tee -a ./moveSmallFiles.sh;
+		fi
+	};export -f FUNCdupscayl;
+	
+	: ${folder:=upscayl_png_ultrasharp_4x} #help
+	find "${folder}/" -iname "*.png" -exec bash -c "FUNCdupscayl '{}'" \;;
+	sort -u ./moveSmallFiles.sh >./moveSmallFilesUnique.sh;
+	trash ./moveSmallFiles.sh
+	chmod +x ./moveSmallFilesUnique.sh
+fi
