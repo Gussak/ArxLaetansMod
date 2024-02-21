35fdb3f0f38d2d4551d32bb4b8f1506abdc2b10b
gussak <ghussack@gmail.com>
2024-02-21 01:46:20 -0300

    upd scripts

--- bin/buildArxLibertatis.sh
+++ bin/buildArxLibertatis.sh
@@ -104,16 +104,19 @@ if echoc -t ${fQuestionDelay} -q "run cmake?";then
 		FUNCpatchCache "CMAKE_BUILD_TYPE:STRING"      "Debug"
 		FUNCpatchCache "CMAKE_CXX_FLAGS_DEBUG:STRING" "-ggdb3 -O0 -fno-omit-frame-pointer -lboost_stacktrace_backtrace" # seems perfect but FPS drops to 3, difficult to test in-game
 		FUNCpatchCache "SET_OPTIMIZATION_FLAGS:BOOL"  OFF  # like -O0 above I guess.  #at build folder. This unoptimizes all the code so breakpoints hit perfectly in nemiver!
+		FUNCpatchCache "CMAKE_EXE_LINKER_FLAGS_DEBUG:STRING" "-rdynamic" # this may cause some rare problems tho
 	else
 		#FUNCpatchCache "CMAKE_BUILD_TYPE:STRING"      ""
 		if echoc -q "Debug (if not will be Release. It may be broken, better test it)?@Dy";then
 			FUNCpatchCache "CMAKE_BUILD_TYPE:STRING"      "Debug"
 			FUNCpatchCache "CMAKE_CXX_FLAGS_DEBUG:STRING" "-g"
 			FUNCpatchCache "SET_OPTIMIZATION_FLAGS:BOOL"  ON
+			FUNCpatchCache "CMAKE_EXE_LINKER_FLAGS_DEBUG:STRING" ""
 		else
 			FUNCpatchCache "CMAKE_BUILD_TYPE:STRING"      "Release"
 			FUNCpatchCache "CMAKE_CXX_FLAGS_DEBUG:STRING" ""
 			FUNCpatchCache "SET_OPTIMIZATION_FLAGS:BOOL"  ON
+			FUNCpatchCache "CMAKE_EXE_LINKER_FLAGS_DEBUG:STRING" ""
 		fi
 	fi
 fi
--- bin/runArxLibertatis.sh
+++ bin/runArxLibertatis.sh
@@ -130,13 +130,15 @@ while true;do
 	
 	###################################
 	## EXPERIMENTAL !!! enabling this may break things! all of these can be enabled dynamically (not sure but may break tho)
+	## better keep all disabled and enable one per time dinamically ex.: copy whole line and paste on console least comment char '#'
+	# env -s ARX_DeveloperModeExperiments_IKnowWhatIAmDoing true	env -s ARX_PrecompileAllowWords true	env -s ARX_PrecompileAllowStaticText true	env -s ARX_PrecompileAllowCommands true	env -s ARX_PrecompileAllowVarNames true
 	###################################
 	: ${ARX_DeveloperModeExperiments_IKnowWhatIAmDoing:=false};export ARX_DeveloperModeExperiments_IKnowWhatIAmDoing # this being true, will allow below experiments to run. if false will deny even if they are true. this && other; looks for cpp usages of g_allowExperiments
 	#export ARX_AllowScriptPreCompilation=false #EXPERIMENTAL
-	export ARX_PrecompileAllowWords=true
-	export ARX_PrecompileAllowStaticText=true
-	export ARX_PrecompileAllowCommands=false # broken
-	export ARX_PrecompileAllowVarNames=false # ?
+	export ARX_PrecompileAllowWords=false
+	export ARX_PrecompileAllowStaticText=false
+	export ARX_PrecompileAllowCommands=false # ?broken
+	export ARX_PrecompileAllowVarNames=false # ?broken
 	export ARX_Allow3DModelsCache=false #EXPERIMENTAL currently broken
 	export ARX_MaxTextureSize=0 #256 # still too glitchy
 	# shall all be coded default false: clear;LC_ALL=C egrep 'ARX_DeveloperModeExperiments_IKnowWhatIAmDoing|ARX_PrecompileAllowStaticText|ARX_PrecompileAllowWords|ARX_Allow3DModelsCache' --include="*.h" --include="*.cpp" -iRnIa * |grep -v false
